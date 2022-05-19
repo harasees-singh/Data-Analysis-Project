9a40220716981924ffa91d2cd7c0bbba0c8e6d82
Harasees Singh <76965050+harasees-singh@users.noreply.github.com>
2022-05-19 13:00:22 +0530

    primary features dashboard done

--- automobile_industry_analysis/src/App.js
+++ automobile_industry_analysis/src/App.js
@@ -12,7 +12,7 @@ function App() {
         {/* <Data /> */}
         {/* <PowerBI /> */}
         <div className='flexme'>
-        <iframe className='report' title="Price" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiZmFlMjgwNzQtNDM1Mi00OTE3LTgzNWEtODZhNmZhN2Y0NTRlIiwidCI6ImE0ZjIzNDc2LWQwMmYtNDJkYS05Y2FjLTBjZWNiYTU3MmNlOSJ9&pageName=ReportSection" frameborder="0" allowFullScreen="true"></iframe></div>
+        <iframe className='report' title="primary features" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiNjY1NmE4ZjEtN2M3Mi00YmMwLWE5NmYtMDdkNmJiMDJkNTRlIiwidCI6ImE0ZjIzNDc2LWQwMmYtNDJkYS05Y2FjLTBjZWNiYTU3MmNlOSJ9&pageName=ReportSection" frameborder="0" allowFullScreen="true"></iframe></div>
       </div>
     </>
   );
